467f15e249b1ac75c1c36716244fede334629154
unknown <v.zelenokor@innopolis.university>
2024-04-22 18:33:35 +0300

    database

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,11 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy.ext.declarative import declarative_base
+import database
 
 from backend.models.user import User
 
 # https://github.com/pranjalpruthi/Streamlit-FastAPI/tree/main
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-Base = declarative_base()
 
 app = FastAPI()
 
