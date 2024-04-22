41dc46b04be73b0ecd5dc7d2caaf643189b9cf35
Vladimir <kvvgames18@gmail.com>
2024-04-22 18:48:10 +0300

    Code refactoring

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,37 @@
-from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.declarative import declarative_base
-import database
+import sqlite3
+import bcrypt
 
+from fastapi import FastAPI, HTTPException
+from backend.database.database import conn, c
 from backend.models.user import User
-
-# https://github.com/pranjalpruthi/Streamlit-FastAPI/tree/main
 
 app = FastAPI()
 
-@app.post("/users/", response_model=User)
-def create_user(name: str, surname: str, email: str):
-    with Session(engine) as session:
-        user = User(name=name, surname=surname, email=email)
-        session.add(user)
-        session.commit()
-        session.refresh(user)
-        return user
+
+@app.post("/user/register")
+def register_user(user: User):
+    hashed_password = bcrypt.hash(user.password)
+    try:
+        # Insert the user into the database
+        c.execute("INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)",
+                  (user.username, user.email, hashed_password))
+        conn.commit()
+        return {"message": "User registered successfully"}
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=400, detail="Username or email already exists")
+
+
+@app.post("/user/login")
+def login_user(user: User):
+    # Check if the user exists in the database
+    c.execute("SELECT * FROM users WHERE email = ?", user.email)
+    stored_user = c.fetchone()
+
+    if stored_user is None:
+        raise HTTPException(status_code=401, detail="User does not exist")
+
+    # Verify the password
+    if not bcrypt.verify(user.password_hash, stored_user[2]):
+        raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+    return {"message": "Login successful", "user_id": stored_user[0]}
