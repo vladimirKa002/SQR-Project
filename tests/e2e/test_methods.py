33bedc343e82e4338ca6b7c1fa5332151092dd45
Vladimir <kvvgames18@gmail.com>
2024-04-29 18:01:38 +0300

    Unit tests auth, config

--- tests/e2e/test_methods.py
+++ tests/e2e/test_methods.py
@@ -1,36 +1,7 @@
-import unittest
-
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
-
-from backend.db import Base, get_db
-from backend.main import app
+from tests.TestProject import TestProject
 
 
-class TestMethods(unittest.TestCase):
-    def setUp(self):
-        SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
-
-        engine = create_engine(
-            SQLALCHEMY_DATABASE_URL,
-            connect_args={"check_same_thread": False},
-            poolclass=StaticPool,
-        )
-        TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-        Base.metadata.create_all(bind=engine)
-
-        def override_get_db():
-            try:
-                db = TestingSessionLocal()
-                yield db
-            finally:
-                db.close()
-
-        app.dependency_overrides[get_db] = override_get_db
-        self.client = TestClient(app)
+class TestMethods(TestProject):
 
     def test_get_all_templates(self):
         response = self.client.get("/template/all")
