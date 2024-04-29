7b914441833fe80ce1b22d0c7b22775c6fe6261c
Pavel Baharuev <pavel5609s@gmail.com>
2024-04-29 23:32:47 +0300

    flake8 fixed

--- frontend/app.py
+++ frontend/app.py
@@ -10,12 +10,13 @@ st.markdown("SQR Team Project")
 st.markdown("""
     **Inno Food Tier List - A Delicious Journey of Exploration!**
 
-    Welcome to Inno Food Tier List, your one-stop shop for creating and exploring the tastiest tier lists around! Built with Streamlit, this
+    Welcome to Inno Food Tier List, your one-stop shop for creating
+    and exploring the tastiest tier lists around! Built with Streamlit, this
     interactive platform empowers you to:
 
     * **Craft Your Own Tier Lists:** Unleash your inner food critic and curate
     personalized tier lists for any food category imaginable.
     * **Save and Store Your Creations:** Preserve your culinary
     masterpieces for posterity and share them with fellow food lovers.
-    * **Learn a new fact every day:** You will know everything about food.        
+    * **Learn a new fact every day:** You will know everything about food.
 """)
